6c6a2db4acf92d36a08c05e9f6c4fe79d55cdc0e
Arkadiusz Rybka <arekrybka@gmail.com>
2017-03-23 23:02:15 +0100

    Code fixes

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,16 @@
 function getTriangleArea(a, h) {
-	if((a <= 0) || (h <= 0)) {
-		return ('Podałeś błędne dane. Wymiary nie mogą być mniejsze bądź równe 0.');
+	var wynik = '';
+	if ((a <= 0) || (h <= 0)) {
+		wynik = 'Podałeś błędne dane. Wymiary nie mogą być mniejsze bądź równe 0.';
+	} else {
+		wynik = (a * h) / 2;
 	}
-	else {
-		var wynik = (a * h) / 2;
-		return wynik;
-	}
+	return wynik;
 }
 
-console.log(getTriangleArea(10, 6));
-
-var firstTriangleArea = getTriangleArea(12, 9),
-	secondTriangleArea = getTriangleArea(-13, 8),
-	thirdTriangleArea = getTriangleArea(7, 5.6);
+var firstTriangleArea = getTriangleArea(12, 9);
+var secondTriangleArea = getTriangleArea(-13, 8);
+var	thirdTriangleArea = getTriangleArea(7, 5.6);
 
 console.log('Pole pierwszego trójkąta wynosi: ' + firstTriangleArea);
 console.log('Pole drugiego trójkąta wynosi: ' + secondTriangleArea);
