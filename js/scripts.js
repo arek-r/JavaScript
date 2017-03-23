7626bec7008c6c03c220ffa820b5ec5204432b73
Arkadiusz Rybka <arekrybka@gmail.com>
2017-03-23 22:30:50 +0100

    Change if conditions

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,6 @@ var value = (a * a) + (2 * a * b) - (b * b);
 
 if (value < 0) {
 	console.log('Wynik jest mniejszy od zera i wynosi: ' + value);
-}
-else if (value > 0) {
-	console.log('Wynik jest większy od zera i wynosi: ' + value);
-}
-else {
-	console.log('Wynik jest równy 0');
+} else {
+	(value > 0) ? console.log('Wynik jest większy od zera i wynosi: ' + value) : console.log('Wynik jest równy 0');
 }
