fdc3548b0256556cd5f29be61f63f666a9862086
Arkadiusz Rybka <arekrybka@gmail.com>
2017-03-25 12:55:11 +0100

    Add split method

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,16 @@
 var text = 'Papugi koszą trawę, unosząc się nad ziemią 15 centymetrów, na czerwonych dywanach.';
 var animal = 'Zielone słonie';
 
-animal = animal.toUpperCase();
-console.log(animal);
+// Remove commas
+for (var i = 0; i < text.length; i++) {
+	var text = text.replace(',', '');
+}
 
-text = text.replace('Papugi', animal);
+var words = text.split(' ');
+
+animal = animal.toUpperCase();
+
+text = text.replace(words[0], animal);
 console.log(text);
 
 var shortText = text.slice(0, (text.length / 2));
